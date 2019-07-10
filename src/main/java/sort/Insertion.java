200f3050107513021bc8922eb42617b1fee313f0
Julian <zhang_yi9527@163.com>
2019-07-10 17:10:59 +0800

    re-learn insertion sort, pick up first element as an ordered sequence, start loop from index 1.

--- src/main/java/sort/Insertion.java
+++ src/main/java/sort/Insertion.java
@@ -3,9 +3,11 @@ package sort;
 import java.util.Optional;
 
 /**
- *  将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
- *  从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。
+ * 将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+ * 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。
  * （如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
+ * 8 4 2 1 0
+ * 4 8 2 1 0
  * @param <T>
  */
 public class Insertion<T extends Comparable<T>> extends Sort<T> {
@@ -15,14 +17,10 @@ public class Insertion<T extends Comparable<T>> extends Sort<T> {
 
         int length = nums.length;
 
-        for (int i = 1; i<length; i++){
-            for (int j = 0; j<i ; j++){
-                if(less(nums[i],nums[j])){
-                    T tmp = nums[i] ;
-                    for(int k = i; k>0;k--) {
-                        nums[k] = nums[k - 1];
-                    }
-                    nums[0]=tmp;
+        for (int i = 1; i < length; i++) {
+            for (int j = i;j>0;j--){
+                if(less(nums[j],nums[j-1])){
+                    swap(nums,j,j-1);
                 }
             }
         }
