acfbe07dfd48031af1749e3267130d8ff5279c4f
Julian <zhang_yi9527@163.com>
2019-10-17 21:30:45 +0800

    first unique character in string

---
+++ src/main/java/string/FirstUniqueCharInString1.java
@@ -0,0 +1,54 @@
+package string;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * First Unique Character in a String
+ * s = "leetcode"
+ * return 0.
+ * <p>
+ * s = "loveleetcode",
+ * return 2.
+ * s = "cc"
+ * return -1
+ * s = "aadadaad"
+ * return -1
+ */
+public class FirstUniqueCharInString1 {
+
+    public int findFirstUniqueChar(String args) {
+        if (args == null || args.length() == 0) {
+            return -1;
+        }
+
+        Map<String, Integer> store = new LinkedHashMap<>();
+
+        char[] strArray = args.toCharArray();
+        for (int i = 0; i < strArray.length; i++) {
+            String si = String.valueOf(strArray[i]);
+            if (store.containsKey(si)) {
+                Integer sum = store.get(si);
+                sum = Integer.sum(sum, 1);
+                store.put(si, sum);
+                continue;
+            }
+            store.put(si, 0);
+
+        }
+
+        String first = "";
+        for (Map.Entry<String, Integer> entry : store.entrySet()) {
+            if (entry.getValue() == 0) {
+                first = entry.getKey();
+                break;
+            }
+        }
+
+        if (!first.equals("")) {
+            return args.indexOf(first);
+        }
+        return -1;
+    }
+
+}
